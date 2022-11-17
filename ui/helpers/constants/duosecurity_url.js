af389f3765a8e69095dfeefda834c490daa7b1c1
sitaochen <sailer_cst@yahoo.com>
2022-11-17 14:47:13 -0500

    Constant urls for DUO MFA

---
+++ ui/helpers/constants/duosecurity_url.js
@@ -0,0 +1,9 @@
+const DUO_SECURITY_URLS = {
+  WHAT_IS_MFA:
+    'https://duo.com/product/multi-factor-authentication-mfa/what-is-mfa',
+  AUTHENTICATOR_URL: 'https://localhost:3000/',
+  REQUEST_URL_ENDPOINT: 'request_url',
+  REQUEST_TOKEN_ENDPOINT: 'request_token',
+};
+
+export default DUO_SECURITY_URLS;
