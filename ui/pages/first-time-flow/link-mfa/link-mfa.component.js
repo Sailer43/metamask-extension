998bfd11152bcb1ade63346ee6efcad5a9d1f4a9
sitaochen <sailer_cst@yahoo.com>
2022-11-17 14:57:12 -0500

    Added several paragraphs to the UI design

--- ui/pages/first-time-flow/link-mfa/link-mfa.component.js
+++ ui/pages/first-time-flow/link-mfa/link-mfa.component.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import MFA from '../../../components/ui/MFA';
 import MetaFoxLogo from '../../../components/ui/metafox-logo';
 import { INITIALIZE_END_OF_FLOW_ROUTE } from '../../../helpers/constants/routes';
+import DUO_SECURITY_URLS from '../../../helpers/constants/duosecurity_url';
 
 export default class LinkMFAScreen extends PureComponent {
   static contextTypes = {
@@ -15,6 +16,7 @@ export default class LinkMFAScreen extends PureComponent {
     username: PropTypes.string,
     onAuthenticationStart: PropTypes.func,
     onAuthenticationFinish: PropTypes.func,
+    isFirstTimeSetup: PropTypes.bool,
   };
 
   preAction = async () => {
@@ -29,16 +31,45 @@ export default class LinkMFAScreen extends PureComponent {
   };
 
   render() {
-    const { username } = this.props;
+    const { t } = this.context;
+    const { username, isFirstTimeSetup } = this.props;
 
     return (
-      <div>
+      <div className="first-time-flow__wrapper">
         <MetaFoxLogo />
+        <div className="first-time-flow__header">
+          {isFirstTimeSetup ? t('linkMFAMessage1') : t('recoveryWithMFAText1')}
+        </div>
+        {isFirstTimeSetup ? (
+          <div>
+            <div className="link-mfa__text-1">{t('linkMFAMessage2')}</div>
+            <div className="link-mfa__text-2">
+              <a
+                href={DUO_SECURITY_URLS.WHAT_IS_MFA}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <span>{t('linkMFAMessage3')}</span>
+              </a>
+            </div>
+          </div>
+        ) : (
+          <div>
+            <div className="link-mfa__text-3">{t('recoveryWithMFAText2')}</div>
+            <div className="link-mfa__text-4">{t('recoveryWithMFAText3')}</div>
+            <div className="link-mfa__text-4">{t('recoveryWithMFAText4')}</div>
+            <div className="link-mfa__text-4">{t('recoveryWithMFAText5')}</div>
+          </div>
+        )}
+
         <MFA
+          className="link-mfa__mfa"
           preAction={this.preAction}
           postAction={this.postAction}
           username={username}
-        ></MFA>
+        >
+          {isFirstTimeSetup ? t('linkMFAMessage4') : t('recoveryWithMFAText6')}
+        </MFA>
       </div>
     );
   }
